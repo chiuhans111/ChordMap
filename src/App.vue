c4a42431c7dcf62de41da5f0bd532c6df357ce54
Hans <chiuhans111@gmail.com>
2023-08-30 17:56:33 +0800

    improve UI

--- src/App.vue
+++ src/App.vue
@@ -6,49 +6,51 @@
 
     </section>
     <section>
-      <div class="piano_container" :style="{ width: fullWidth + 'px' }">
-        <!-- Note Name -->
-        <div class="piano_container-note_name">
-          <div :style="key.containerStyle" v-for="key in pianoKeys" :key="key.index">
-            <span>
-              {{ key.name }}
-            </span>
-            <span class="note_octave">
-              {{ key.octave }}
-            </span>
+      <div class="scroll_horizontal">
 
+        <div class="piano_container" :style="{ width: fullWidth + 'px' }">
+          <!-- Note Name -->
+          <div class="piano_container-note_name">
+            <div :style="key.containerStyle" v-for="key in pianoKeys" :key="key.index">
+              <span>
+                {{ key.name }}
+              </span>
+              <span class="note_octave">
+                {{ key.octave }}
+              </span>
+
+            </div>
           </div>
-        </div>
-        <!-- keyboard -->
-        <div class="piano_container-keyboard">
-          <div :style="key.keyStyle" :class="key.cssClass" class="note_body" v-for="key in pianoKeys" :key="key.index" @click.prevent="pianoKeyClick(key)">
-            <!-- {{ key.name }} -->
+          <!-- keyboard -->
+          <div class="piano_container-keyboard">
+            <div :style="key.keyStyle" :class="key.cssClass" class="note_body" v-for="key in pianoKeys" :key="key.index" @click.prevent="pianoKeyClick(key)">
+              <!-- {{ key.name }} -->
+            </div>
           </div>
+          <!-- Working area -->
+          <div class="piano_container-editor">
+            <div :style="key.containerStyle" :class="key.cssClass" class="note_box" v-for="key in pianoKeys" :key="key.index" @click.prevent="editorKeyClick(key)">
+              <div :class="{ 'note_enable': key.enable }" class="note_box-tag">
+                {{ key.name }}
+                <!-- {{ key.enable }} -->
+              </div>
+            </div>
+          </div>
+
         </div>
-        <!-- Working area -->
-        <div class="piano_container-editor">
-          <div :style="key.containerStyle" :class="key.cssClass" class="note_box" v-for="key in pianoKeys" :key="key.index" @click.prevent="editorKeyClick(key)">
-            <div :class="{ 'note_enable': key.enable }" class="note_box-tag">
-              {{ key.name }}
-              <!-- {{ key.enable }} -->
+
+        <!-- Result Area -->
+        <div class="ratio_hint" :style="{ width: fullWidth + 'px' }">
+          <div class="ratio_hint-row" :class="{ 'harmonized': hint.harmonized }" v-for="hint, i in analyzeResult.hints" :key="i" @click="playHint(hint)">
+            <div class="ratio_hint-node" :style="node.style" :class="node.cssClass" v-for="node, i in hint.nodes" :key="i">
+              <template v-if="!node.cssClass['node_is_minimize']">
+                {{ node.ratio }}
+              </template>
             </div>
           </div>
         </div>
 
       </div>
-    </section>
-    <section>
-      <!-- Result Area -->
-      <div class="ratio_hint">
-        <div class="ratio_hint-row" :class="{ 'harmonized': hint.harmonized }" v-for="hint, i in analyzeResult.hints" :key="i" @click="playHint(hint)">
-          <div class="ratio_hint-node" :style="node.style" :class="node.cssClass" v-for="node, i in hint.nodes" :key="i">
-            <template v-if="!node.cssClass['node_is_minimize']">
-              {{ node.ratio }}
-            </template>
-          </div>
-        </div>
-      </div>
-
     </section>
   </div>
 </template>
